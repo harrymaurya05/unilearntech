6006e9ac8be2b3ccfa26b89b9e74df90ae8cad00
hariomaurya <hariom.maurya@unicommerce.com>
2022-03-03 19:46:30 +0530

    project add

---
+++ src/main/java/com/unilearntech/unilearntech/UnilearntechApplication.java
@@ -0,0 +1,13 @@
+package com.unilearntech.unilearntech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnilearntechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnilearntechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unilearntech/unilearntech/UnilearntechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unilearntech.unilearntech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnilearntechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
